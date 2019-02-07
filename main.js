06fe61fa475d649e581c719c05a8dc9a214e3201
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-02-07 15:03:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ new Vue({
     },
     methods: {
         loadEvents() {
-            fetch('https://cors.io/?https://api.meetup.com/GDG-Jalandhar/events?photo-host=public&page=20&sig_id=225657600&status=past&sig=63d9475bd43b35e91b46b4ef6682ecd846094015')
+            fetch('API')
             .then(data => data.json())
             .then(data => {
                 this.events = data;
